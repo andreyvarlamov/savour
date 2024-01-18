083655527683de444d14d5ffded0da86ad973cae
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2024-01-17 19:59:48 -0800

    Software rendering using renderer and texture

--- source/sdl_savour.cpp
+++ source/sdl_savour.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 
 #include <sdl2/SDL.h>
 
@@ -13,20 +14,24 @@ int main(int argc, char **argv)
     SDL_Window *Window = SDL_CreateWindow("Savour",
                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                           1920, 1080,
-                                          SDL_WINDOW_SHOWN);
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     Assert(Window);
 
-    SDL_Surface *WindowSurface = SDL_GetWindowSurface(Window);
+    SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-    SDL_FillRect(WindowSurface, NULL, SDL_MapRGB(WindowSurface->format, 0x00, 0x00, 0x00));
+    u32 Width = 32;
+    u32 Height = 24;
+    SDL_Texture *ScreenTexture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, Width, Height);
 
-    SDL_UpdateWindowSurface(Window);
+    u32 *Pixels = (u32 *) calloc(1, Width * Height * 4);
 
-    u32 *Pixels = (u32 *) WindowSurface->pixels;
-    u32 PixelCount = WindowSurface->w * WindowSurface->h;
+    SDL_SetWindowMinimumSize(Window, Width, Height);
+
+    SDL_RenderSetLogicalSize(Renderer, Width, Height);
 
     i32 FrameCount = 0;
-    b32 IsGreen = false;
+
+    srand((u32)time(NULL));
 
     b32 ShouldQuit = false;
     while (!ShouldQuit)
@@ -43,27 +48,28 @@ int main(int argc, char **argv)
             }
         }
 
-
         if (FrameCount > 100)
         {
-            u32 PixelColor = IsGreen ? 0x00FF00FF : 0xFF0000FF;
             u32 *PixelCursor = Pixels;
             for (u32 PixelIndex = 0;
-                 PixelIndex < PixelCount;
+                 PixelIndex < Width * Height;
                  ++PixelIndex)
             {
-                *PixelCursor++ = PixelColor;
+                b32 IsGreen = rand() % 2;
+                *PixelCursor++ = IsGreen ? 0x00FF00FF : 0xFF0000FF;
             }
 
-            IsGreen = !IsGreen;
             FrameCount = 0;
         }
         else
         {
             FrameCount++;
         }
-            
-        SDL_UpdateWindowSurface(Window);
+
+        SDL_RenderClear(Renderer);
+        SDL_UpdateTexture(ScreenTexture, NULL, Pixels, Width * 4);
+        SDL_RenderCopy(Renderer, ScreenTexture, NULL, NULL);
+        SDL_RenderPresent(Renderer);
     }
 
     SDL_DestroyWindow(Window);
